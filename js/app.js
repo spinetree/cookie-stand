cf8d41058dfdd9afc05740a7f6b01a7fdb6953ec
spinetree <steve@spinetree.org>
2019-08-12 17:33:53 -0700

    salestable ready to fill, functions working to project

--- js/app.js
+++ js/app.js
@@ -11,31 +11,85 @@ var store1 = {
     hourlySales: []
 };
 
+var store2 = {
+    storeId: 2,
+    storeName: 'SeaTac Airport',
+    minCust: 3,
+    maxCust: 24,
+    cookiesPerCust: 1.2,
+    hourlySales: []
+};
+
+var store3 = {
+    storeId: 3,
+    storeName: 'Seattle Center',
+    minCust: 11,
+    maxCust: 38,
+    cookiesPerCust: 3.7,
+    hourlySales: []
+};
+
+var store4 = {
+    storeId: 4,
+    storeName: 'Capitol Hill',
+    minCust: 20,
+    maxCust: 38,
+    cookiesPerCust: 2.3,
+    hourlySales: []
+};
+
+var store5 = {
+    storeId: 5,
+    storeName: 'Alki',
+    minCust: 2,
+    maxCust: 16,
+    cookiesPerCust: 4.6,
+    hourlySales: []
+};
+
+// do this programattically later if time
+// function niceTime() {
+    // for (var i=0; i < times.length; i++;) {
+
+    // }
+// }
+
+var times = ['6 a.m.','7 a.m.','8 a.m.','9 a.m.','10 a.m.','11 a.m.','12 p.m.', '1 p.m.','2 p.m.','3 p.m.','4 p.m.','5 p.m.','6 p.m.','7 p.m.','8 p.m.'];
+
 function projHourly(store) {
 
     var min = store.minCust;
     var max = store.maxCust;
     var avg = store.cookiesPerCust;
-    var hourlySales = []; 
+    var hourlySales = [];
 
     function customers(min, max) {
-        return Math.floor(Math.random() * (max-min) + min);
+        return Math.floor(Math.random() * (max - min) + min);
     };
 
-    for (var i = 600; i <= 2000; i += 100) {
-        var sales = Math.floor(customers(min,max) * avg);  
-        hourlySales.push(sales);   
-    }     
+    for (var i = 0; i < times.length; i++) {
+        var sales = Math.floor(customers(min, max) * avg);
+        hourlySales.push(sales);
+    }
 
     store.hourlySales = hourlySales;
 }
+
+
+function listHourly(store) {
+
+    for (var i = 0; i < times.length; i++) {
+        var listItem = (times[i] + ' : ' + store.hourlySales[i]);
+        console.log(listItem);
+    }
+};
 
 
 
 // separate these functions out into a global function and not an object method.
 
 // generate random number customers per hour betwen these then
-// calculate projected cookees sold each hour between 6am and 8pm
+// calculate projected cookees sold each hour between 6a.m. and 8pm
 
 // edge cases: (stretch goal) allows for array containing key/value pairs for holidays? Nah, that should be thing where it takes date as input and has an object that's teh table of dates with their averages in them. Doesn't matter when it's a holidy, it should eventually learn what every day's average is.  
 
