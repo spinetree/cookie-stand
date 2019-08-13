be8883fc04983f7a1521e07655879ea460b19989
spinetree <steve@spinetree.org>
2019-08-13 14:47:17 -0700

    finally done, egad that was hairy, but learned a lot. Fml tho.

--- js/app.js
+++ js/app.js
@@ -50,14 +50,14 @@ var store5 = {
 
 // hard code times for now
 // do this programattically later if time
-var times = ['6 a.m.', '7 a.m.', '8 a.m.', '9 a.m.', '10 a.m.', '11 a.m.', '12 p.m.', '1 p.m.', '2 p.m.', '3 p.m.', '4 p.m.', '5 p.m.', '6 p.m.', '7 p.m.', '8 p.m.'];
+var times = ['6 am', '7 am', '8 am', '9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm', '8 pm'];
 
 // hard code store list for now
 var stores = [store1, store2, store3, store4, store5];
 
 // generate the hourly projections and insert them in each store object as an array
 
-function projHourly(store) {
+function projSales(store) {
 
     var min = store.minCust;
     var max = store.maxCust;
@@ -67,21 +67,26 @@ function projHourly(store) {
     function customers(min, max) {
         return Math.floor(Math.random() * (max - min) + min);
     };
+    
     for (var i = 0; i < times.length; i++) {
         var sales = Math.floor(customers(min, max) * avg);
         hourlySales.push(sales);
-    }
+    };
+
     store.hourlySales = hourlySales;
+        
+    function dailySales(hourlySales) {
+        var dailySales = 0;
+        for (var x = 0; x < hourlySales.length; x++) {
+             dailySales += hourlySales[x];  
+        };
+        return dailySales;
+    };
+
+    store.dailySales = dailySales(hourlySales);
+    // console.log(store.dailySales)
+
 }
-
-function initialize() {
-
-    for (var i = 0; i < stores.length; i++) {
-        projHourly(stores[i]);
-        console.log('initialized');
-    }
-};
-
 
 // spit out list items from what's in the store's hourlySales array
 
@@ -92,23 +97,36 @@ function listHourly(store) {
     }
 };
 
+
+function initialize() {
+
+    for (var i = 0; i < stores.length; i++) {
+        projSales(stores[i]);
+    }
+};
+
 // make the store h3 and ul
 
 function renderStores() {
 
-    var salestable = document.getElementById('test');
+    var salestable = document.getElementById('sales-table');
 
     for (var i = 0; i < stores.length; i++) {
         
         var hourlySales = stores[i].hourlySales;
         
         var storeUl = document.createElement('ul');
-        
+
         for (var x = 0; x < hourlySales.length; x++) {    
             var li = document.createElement('li');
-            li.textContent = ('some placeholder data: ' + x);
+            li.textContent = (times[x] + ': ' + hourlySales[x]);
             storeUl.appendChild(li);
         };
+
+        var totalLi = document.createElement('li');
+        totalLi.textContent = ('Total: ' + stores[i].dailySales); 
+        totalLi.setAttribute('class','total');
+        storeUl.appendChild(totalLi);
 
         var storeh3 = document.createElement('h3');
         storeh3.textContent = stores[i].storeName;
@@ -116,7 +134,7 @@ function renderStores() {
         var storeDiv = document.createElement('div');
         storeDiv.setAttribute('id', 'store-' + stores[i].storeId);
 
-        //let's append things together while they're still easy variables in the ether instead of after they've become DOM objects ffs
+        //let's append things together while they're still easy variables instead of after they've become DOM objects ffs
         storeDiv.appendChild(storeh3);
         storeDiv.appendChild(storeUl);
         salestable.appendChild(storeDiv);
@@ -125,31 +143,8 @@ function renderStores() {
 
 }
 
-
-// populate the store ul with the list data
-function addToList(storeId) {}
-
-
-// function render() {
-
-//     for (var i = 0; i < stores.length; i++) {
-//         projHourly(stores[i]);
-//         // console.log(i, stores[i].storeId, stores[i].storeName);
-//         renderStore(i, stores[i].storeId, stores[i].storeName);
-//     }
-// };
-
-// will need to do a for loop in another for loop to make this work - loop through locations then loop through again, grabbing an hour from the hours glonal array and the sales number from the store object's projected sales array
-
-
-// for each (store in list) {x
-// spit out an h3
-// spit out an empty ul
-// for each (item in projHourly)
-//    { spit out an li
-//      append to the ul
-//      }
-// }
+initialize();
+renderStores();
 
 
 // for each store 
@@ -162,20 +157,3 @@ function addToList(storeId) {}
 
 
 
-
-
-// generate random number customers per hour betwen these then
-// calculate projected cookees sold each hour between 6a.m. and 8pm
-
-// edge cases: (stretch goal) allows for array containing key/value pairs for holidays? Nah, that should be thing where it takes date as input and has an object that's teh table of dates with their averages in them. Doesn't matter when it's a holidy, it should eventually learn what every day's average is.  
-
-
-// generate a header with store name etc.
-
-// generate li for each hourly total between 6am and 8pm
-// for each store
-// generate a ul named that storename
-// iterate through the sales figures
-// for each hour generate an li
-// contains: hour, projected sales number (tricky to go up to 12 then swap am pm) (Do this with 24hour format?)
-// append that li to the ul 
