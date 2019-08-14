a3e8b9a3d9c1c532d4ca0676689513e6c76d6eaf
spinetree <steve@spinetree.org>
2019-08-14 15:18:37 -0700

    tuesday good

--- js/app.js
+++ js/app.js
@@ -2,8 +2,9 @@
 
 // console.log('js loading');
 
-var times = ['6 am', '7 am', '8 am', '9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm', '8 pm'];
 Store.storeList = [];
+var times = ['6 am', '7 am', '8 am', '9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm', '8 pm'];
+var salesTable = document.getElementById('sales-table');
 
 function Store(name, minCust, maxCust, cookiesPerCust) {
     this.name = name;
@@ -12,49 +13,49 @@ function Store(name, minCust, maxCust, cookiesPerCust) {
     this.cookiesPerCust = cookiesPerCust;
     this.hourlySales = [];
     Store.storeList.push(this);
-
+    
     this.projectSales = function () {
-
+        
         var min = this.minCust;
         var max = this.maxCust;
         var avg = this.cookiesPerCust;
-
+        
         function customers(min, max) {
             return Math.floor(Math.random() * (max - min) + min);
         };
-
+        
         for (var i = 0; i < times.length; i++) {
             var sales = Math.floor(customers(min, max) * avg);
             this.hourlySales.push(sales);
         };
     };
-
+    
     this.totalSales = function () {
-
+        
         var dailyTotal = 0;
         for (var x = 0; x < this.hourlySales.length; x++) {
             dailyTotal += this.hourlySales[x];
         };
         this.dailyTotal = dailyTotal;
-
+        
     };
-
+    
     this.projectSales();
     this.totalSales();
-
+    
     this.renderRow = function () {
-
+        
         var rowArray = [];
         var name = this.name;
         var hourlySales = this.hourlySales;
         var dailyTotal = this.dailyTotal;
-
+        
         rowArray.unshift(name);
         for (var i = 0; i < hourlySales.length; i++) {
             rowArray.push(hourlySales[i]);
         };
         rowArray.push(dailyTotal);
-
+        
         return rowArray;
     };
 };
@@ -69,7 +70,7 @@ function getHourlyTotals() {
         };
         hourlyTotals.push(sum);
     };
-}
+};
 
 var firstPike = new Store('1st and Pike', 23, 65, 2.3);
 var SeaTac = new Store('SeaTac Airport', 3, 24, 2.3);
@@ -77,8 +78,29 @@ var seattleCenter = new Store('SeattleCenter', 11, 38, 3.7);
 var capitolHill = new Store('Capitol Hill', 20, 38, 2.3);
 var Alki = new Store('Alki', 2, 16, 4.6);
 
+
+function renderHeader() {
+    var header = document.createElement('thead');
+    var headerRow = document.createElement('tr');
+    header.appendChild(headerRow);
+    var headerCell = document.createElement('th');
+    header.appendChild(headerCell);
+    
+    for(var i=0; i <= times.length; i++) {
+        var headerCell = document.createElement('th');
+        headerCell.innerText = times[i];
+        header.appendChild(headerCell);
+        };
+    
+    var lastHeaderCell = document.createElement('th');
+    headerCell.innerText = 'Daily Total';
+    header.appendChild(headerCell);
+
+    salesTable.appendChild(header);    
+};
+
 function renderStores() {
-    var salesTable = document.getElementById('sales-table');
+    var tbody = document.createElement('tbody');
 
     for (var i = 0; i < Store.storeList.length; i++) {
         var rowContent = Store.storeList[i].renderRow();
@@ -88,26 +110,27 @@ function renderStores() {
             td.innerHTML = rowContent[x];
             row.appendChild(td);
         };
-        salesTable.appendChild(row);
+        tbody.appendChild(row);
     };
+    salesTable.appendChild(tbody);
 
-    //render the totals
+    var footer = document.createElement('tfoot');    
     var totalsRow = document.createElement('tr');
     //one blank td for the store names
     var dailyTotalLabel = document.createElement('td');
-    dailyTotalLabel.innerHTML = 'Hourly Total';
+    dailyTotalLabel.innerText = 'Hourly Total';
     totalsRow.appendChild(dailyTotalLabel);
-
 
     for (var x = 0; x < hourlyTotals.length; x++) {
         var totalCell = document.createElement('td');
         totalCell.innerHTML = hourlyTotals[x];
         totalsRow.appendChild(totalCell);
     };
-    console.log(totalsRow);
-    salesTable.appendChild(totalsRow);
+    footer.appendChild(totalsRow);
+    salesTable.append(footer);
 };
 
+renderHeader();
 getHourlyTotals();
 renderStores();
 
